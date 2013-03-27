2b85f4e9a3672114d38154a2f488416377985c90
Jake Bailey <rjacob@cs.washington.edu>
2013-03-26 17:28:55 -0700

    Untabify TimelapseBreakpointScanner.js, TimelapseBreakpointTracker.js, TimelapseOverview.js.

--- Source/WebCore/inspector/front-end/TimelapseBreakpointTracker.js
+++ Source/WebCore/inspector/front-end/TimelapseBreakpointTracker.js
@@ -1,3 +1,34 @@
+/*
+ *  Copyright (C) 2013, Brian Burg, Jake Bailey.
+ *  Copyright (C) 2013, University of Washington. All rights reserved.
+ *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1.  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ * 2.  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ * 3.  Neither the name of the University of Washington nor the names of
+ *     its contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 /**
  * @constructor
  */
@@ -111,12 +142,12 @@ WebInspector.TimelapseBreakpointTracker.prototype = {
     // Callbacks from TimelapseModel
     _playbackWillStart: function()
     {
-	this._endPendingInterval();
+        this._endPendingInterval();
 
-	var markIndex = this._model.replayStartMarkIndex;
-	var canHitBreakpoints = WebInspector.debuggerModel.debuggerEnabled() && WebInspector.debuggerModel.breakpointsActive();
-	if (canHitBreakpoints)
-	    this._exploredIntervals.startInterval(markIndex);
+        var markIndex = this._model.replayStartMarkIndex;
+        var canHitBreakpoints = WebInspector.debuggerModel.debuggerEnabled() && WebInspector.debuggerModel.breakpointsActive();
+        if (canHitBreakpoints)
+            this._exploredIntervals.startInterval(markIndex);
     },
 
     _inputHit: function(event)
@@ -131,7 +162,7 @@ WebInspector.TimelapseBreakpointTracker.prototype = {
         // ???: is this actually needed?
         this._endPendingInterval();
     
-      	var rawLocation = event.data.callFrames[0].location;
+        var rawLocation = event.data.callFrames[0].location;
         var uiLocation = WebInspector.debuggerModel.rawLocationToUILocation(rawLocation);
         var lineNumber = rawLocation.lineNumber;
         var breakpoint = WebInspector.breakpointManager.findBreakpoint(uiLocation.uiSourceCode, lineNumber);
