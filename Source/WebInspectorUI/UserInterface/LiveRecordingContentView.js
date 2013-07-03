7f73beb420ee38e24d0a241fba8c345cd4d4ac7a
Katie Madonna <katieem@att.net>
2013-07-03 13:59:47 -0700

    Make LiveRecordingContentView display live inputs in ticker-tape.

--- Source/WebInspectorUI/UserInterface/LiveRecordingContentView.js
+++ Source/WebInspectorUI/UserInterface/LiveRecordingContentView.js
@@ -36,15 +36,25 @@ WebInspector.LiveRecordingContentView = function(recording)
 
     this._canvas = this.element.appendChild(document.createElement("canvas"));
 
+    this._timeline = { provider: null, maxIndex: -1, data: [] };
+
     this._listeners = new WebInspector.EventListenerGroup("LiveRecordingContentView recording listeners");
     this._listeners.install();
+
+    this._animateFrameCallback = this.animateFrame.bind(this);
+
+    window.webkitRequestAnimationFrame(this._animateFrameCallback);
 };
 
 WebInspector.LiveRecordingContentView.StyleClassName = "live-recording";
 
 WebInspector.LiveRecordingContentView.GraphBackgroundColor = new WebInspector.Color("#fff");
 WebInspector.LiveRecordingContentView.GraphBorderWidth = 1; // in pixels
-WebInspector.LiveRecordingContentView.BorderStrokeColor = new WebInspector.Color("#666");
+WebInspector.LiveRecordingContentView.GraphBorderStrokeColor = new WebInspector.Color("#666");
+WebInspector.LiveRecordingContentView.MaxRecordLifetime = 10.0; // seconds
+WebInspector.LiveRecordingContentView.MaxBinsPerTimeline = 600;
+WebInspector.LiveRecordingContentView.LineGraphFillColor = new WebInspector.Color.fromRGBA(8,32,130,0.6);
+
 
 WebInspector.LiveRecordingContentView.prototype = {
     constructor: WebInspector.LiveRecordingContentView,
@@ -64,6 +74,15 @@ WebInspector.LiveRecordingContentView.prototype = {
         this._listeners.uninstall(true);
     },
 
+    animateFrame: function()
+    {
+        this._recomputeTimeline();
+        this._drawGraph();
+
+        if (this._recording.isCapturing)
+            window.webkitRequestAnimationFrame(this._animateFrameCallback);
+    },
+
     // Private
 
     _autosizeCanvas: function()
@@ -73,6 +92,68 @@ WebInspector.LiveRecordingContentView.prototype = {
         this._canvas.height = this.element.clientHeight;
         this._canvas.style.height = this.element.clientHeight + 'px';
         this._cachedOffsetWidth = this.element.offsetWidth;
+    },
+
+    // Clear the data of the timeline.
+    _resetTimeline: function()
+    {
+        this._timeline.maxIndex = -1;
+        this._timeline.data = [];
+    },
+
+    _recomputeTimeline: function()
+    {
+        var inputProvider = this._recording.firstProviderWithConstructor(WebInspector.LiveInputDataProvider);
+        this._timeline.provider = inputProvider;
+        var inputs = inputProvider.inputs;
+
+        if (!inputs[0])
+            return;
+
+        this._binsPerTimeline = Math.min(this._cachedOffsetWidth/2, WebInspector.LiveRecordingContentView.MaxBinsPerTimeline);
+
+        var interval = WebInspector.LiveRecordingContentView.MaxRecordLifetime;
+        var now = Date.now();
+        if (!this._previousAnimationTime)
+            this._minTimestamp = inputs[0].timestamp - interval;
+        else
+            this._minTimestamp = this._minTimestamp + (now - this._previousAnimationTime) * 0.001;
+
+        this._previousAnimationTime = now;
+        var timestampGranularity = interval / this._binsPerTimeline;
+        this._resetTimeline();
+
+        // Create sparse arrays with 101 cells each to fill with counts for a given group.
+        function markPercentagesForRecord(record)
+        {
+            if (record.timestamp < this._minTimestamp)
+                return false;
+
+            var snappedTimestamp = record.timestamp - (record.timestamp % timestampGranularity);
+            var percent = Math.round(this._binsPerTimeline * (snappedTimestamp - this._minTimestamp) / interval);
+            var percentile = Number.constrain(percent, 0, this._binsPerTimeline-1);
+
+            if (!this._timeline.data[percentile])
+                this._timeline.data[percentile] = 1;
+            else
+                this._timeline.data[percentile] += 1;
+
+            return true;
+        }
+        var i = 0;
+        for (i = inputs.length-1; i >= 0; i--) {
+            if (!markPercentagesForRecord.call(this, inputs[i]))
+                break;
+        }
+
+        var timeline = this._timeline;
+        var highMark = 0;
+        for (var i = 0; i < timeline.data.length; i++) {
+            if (timeline.data[i] > highMark && i < timeline.data.length*0.9) {
+                highMark = timeline.data[i];
+                timeline.maxIndex = i;
+            }
+        }
     },
 
     _clearGraph: function(ctx)
@@ -87,7 +168,7 @@ WebInspector.LiveRecordingContentView.prototype = {
         ctx.clearRect(0, 0, availWidth, availHeight);
 
         // Draw border.
-        ctx.strokeStyle = WebInspector.LiveRecordingContentView.BorderStrokeColor.value;
+        ctx.strokeStyle = WebInspector.LiveRecordingContentView.GraphBorderStrokeColor.value;
         ctx.lineWidth = WebInspector.LiveRecordingContentView.GraphBorderWidth;
         ctx.beginPath();
         ctx.moveTo(0, 0);
@@ -95,6 +176,7 @@ WebInspector.LiveRecordingContentView.prototype = {
         ctx.lineTo(availWidth, availHeight);
         ctx.lineTo(0, availHeight);
         ctx.lineTo(0, 0);
+        ctx.fill();
         ctx.stroke();
     },
 
@@ -102,5 +184,39 @@ WebInspector.LiveRecordingContentView.prototype = {
     {
         var ctx = this._canvas.getContext('2d');
         this._clearGraph(ctx);
+
+        if (!this._timeline.data || !this._timeline.provider)
+            return;
+
+        ctx.save();
+        ctx.translate(this._graphBorderWidth, this._graphBorderWidth);
+
+        var pointCount = this._binsPerTimeline;
+        var availHeight = this._canvas.height - WebInspector.LiveRecordingContentView.GraphBorderWidth*2+1;
+        var availWidth = this._canvas.width - WebInspector.LiveRecordingContentView.GraphBorderWidth*2+1;
+        var offsetPerPoint = availWidth / pointCount;
+        var maxValue = this._timeline.data[this._timeline.maxIndex];
+
+        // Draw bars for all actions.
+        function drawLineGraph(data) {
+            ctx.lineJoin = "round";
+            ctx.beginPath();
+            ctx.moveTo(0, availHeight);
+            var highMark = 0;
+            for (var i = 0; i < pointCount; i++) {
+                var percent = (data[i] / maxValue) || 0;
+                var pointX = offsetPerPoint*i+offsetPerPoint/2;
+                var pointY = availHeight * (1-percent);
+                ctx.lineTo(pointX, pointY);
+            }
+            ctx.lineTo(availWidth, availHeight);
+            ctx.lineTo(0, availHeight);
+            ctx.closePath();
+            ctx.fill();
+        }
+
+        var currentData = this._timeline.data.slice();
+        ctx.fillStyle = WebInspector.LiveRecordingContentView.LineGraphFillColor.value;
+        drawLineGraph.call(this, currentData);
     }
 };
